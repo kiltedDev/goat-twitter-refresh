0a6eb3011ce86fc013dc31d20bca939c4834758e
Thomas Wilson <twilson.spqr@gmail.com>
2018-01-15 20:27:29 -0500

    first commit

---
+++ src/components/TwitterFeed.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Tweet from './Tweet'
+
+class TwitterFeed extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tweets: props.tweets
+    }
+  }
+
+  render() {
+    let tweets = this.state.tweets.map(tweet => {
+      return <Tweet key={tweet.id_str} tweet={tweet} />
+    })
+
+    return (
+      <div className="row columns small-12 medium-9 large-6">
+        <div className="twitter-feed">
+          {tweets}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TwitterFeed;
